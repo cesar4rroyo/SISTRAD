4a29c52d8b921d93ff6da7d1e39473df13c224cf
unknown <dracbolteon@gmail.com>
2021-03-02 22:06:36 -0500

    cambios carpeta public

--- public/js/funciones2.js
+++ public/js/funciones2.js
@@ -619,3 +619,38 @@ function mostrarEscuelas(ruta, entidad, tipo) {
 		}
 	});
 }
+
+function filter(__val__){
+	var preg = /^([0-9]+\.?[0-9]{0,2})$/; 
+	if(preg.test(__val__) === true){
+		return true;
+	}else{
+	return false;
+	}
+}
+
+function filterFloat(evt, input) {
+    // Backspace = 8, Enter = 13, ‘0′ = 48, ‘9′ = 57, ‘.’ = 46, ‘-’ = 43
+    var key = window.Event ? evt.which : evt.keyCode;
+    var chark = String.fromCharCode(key);
+    var tempValue = input.value + chark;
+    if (key >= 48 && key <= 57) {
+        if (filter(tempValue) === false) {
+            return false;
+        } else {
+            return true;
+        }
+    } else {
+        if (key == 8 || key == 13 || key == 0) {
+            return true;
+        } else if (key == 46) {
+            if (filter(tempValue) === false) {
+                return false;
+            } else {
+                return true;
+            }
+        } else {
+            return false;
+        }
+    }
+}
